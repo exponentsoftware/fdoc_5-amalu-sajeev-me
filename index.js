b1dbc364fb2ded7fdf1cf28b6a6ce3ed8af3490a
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-03-12 15:17:52 +0530

    q&a

--- index.js
+++ index.js
@@ -63,3 +63,63 @@ const tenMostlargestCountries = await fetch(API_URL).then(data => {
 });
 
 console.log(tenMostlargestCountries);
+
+
+/**
+ * Explain the following questions in your own words
+ */
+/*
+ * What is the difference between forEach, map, filter and reduce ? 
+
+forEach() just loop through the elements. 
+It's throws away return values and always returns undefined. 
+The result of this method does not give us an output .
+
+map() loop through the elements and stores return values 
+by iterating main array
+
+filter() also loop through the elements but only returns 
+the elements in the index where the callback function returns true;
+
+reduce() takes all of the elements in an array and 
+reduces them into a single value.
+
+
+ * Explain these using your own words Explain the difference between 
+   function declaration and arrow function ? 
+
+Arrow functions do not have an arguments binding.
+Unlike regular functions, arrow functions do not have their own this
+Regular functions created using function declarations or expressions 
+are constructible and callable. the arrow functions are only callable 
+and not constructible.
+
+
+ * Explain the difference between find and findIndex ? 
+
+the find method returns the element value, 
+but the findIndex method returns the element index.
+
+ * If you like to filter out one object element in an array 
+ * which method do you like to use: filter or find ? 
+
+find();
+
+ * Explain Explain the difference of var, let and const 
+ * when we declare a variable ?
+
+The scope of a var variable is functional scope. 
+The scope of a let & const variable is block scope.
+var can be updated and re-declared into the scope.
+let can be updated but cannot be re-declared into the scope.
+const cannot be updated or re-declared into the scope.
+var & let can be declared without initialization. 
+const cannot be declared without initialization.
+var can be accessed without initialization as its default value is “undefined”.
+let cannot be accessed without initialization, as it returns an error.
+const cannot be accessed without initialization, 
+as it cannot be declared without initialization.
+
+
+
+ */
